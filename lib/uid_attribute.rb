613218222230fa2e39e13a4b686bf459afec5498
Paul Belt <paul.belt@gmail.com>
2010-05-06 17:53:32 -0400

    Revert "fix initialization sequence;remove redundant calls"
    This reverts commit ebe771b8d7d6f2b636811a78ef436dcdb227ca73.

--- lib/uid_attribute.rb
+++ lib/uid_attribute.rb
@@ -11,12 +11,6 @@ module ClassMethods
 
   def install_uid_attribute #:nodoc:
     uid_attribute
-
-    # used by rails
-    def initialize(args) # :nodoc:
-      super args
-      set_uid
-    end
   end
 
   # :call-seq:
@@ -27,6 +21,7 @@ module ClassMethods
   def uid_attribute(uid_attr = :uid)
     if ancestors.include?('ActiveRecord::Base')
       validates_uniqueness_of uid_attr
+      before_validation :set_uid
     end
 
     class_eval("class << self;attr_accessor :uid_attr;attr_accessor :uid_object; end")
@@ -36,10 +31,16 @@ module ClassMethods
 
 end
 
-# used by ruby
-def initialize # :nodoc:
-  super
-  set_uid
+if self.class.ancestors.include?('ActiveRecord::Base')
+  def initialize(args) # :nodoc:
+    super args
+    self.set_uid
+  end
+else
+  def initialize # :nodoc:
+    super
+    self.set_uid
+  end
 end
 
 # :call-seq:
@@ -48,7 +49,8 @@ end
 # This function sets the attribute (as identified by klass.uid_attribute)
 
 def set_uid
-  raise "dev.error: #{self.class}.respond_to?(:#{self.class.uid_attr}) == false" unless respond_to?(self.class.uid_attr)
+  raise "dev.error: #{self.class}.respond_to?(:#{self.class.uid_attr}) == false" unless self.respond_to?(self.class.uid_attr)
+  #return unless self.send("#{self.class.uid_attr}=").nil?
 
   if self.class.uid_object
     uid = UUIDTools::UUID.md5_create(UUIDTools::UUID_OID_NAMESPACE, "#{self.inspect}")
@@ -56,7 +58,7 @@ def set_uid
     uid = UUIDTools::UUID.random_create.to_s
   end
 
-  send("#{self.class.uid_attr}=", uid)
+  self.send("#{self.class.uid_attr}=", uid)
 end
 
 # /module
